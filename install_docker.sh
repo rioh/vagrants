8d82c1b29bcaaa9f36eb38bf065d143c3f9fb2c0
M L <sapient007@hotmail.com>
2017-10-20 14:03:12 -0400

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,4 +1,4 @@
-     sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+     sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys <removed key file>
      sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
      sudo apt-get update
      sudo apt-get install -y docker-engine --allow-unauthenticated
